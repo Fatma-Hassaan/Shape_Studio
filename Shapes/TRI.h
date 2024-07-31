bef00b6746310d472ef6691568c7e8ac07993567
fatma hassaan <138074800+Fatma-Hassaan@users.noreply.github.com>
2024-07-31 15:34:08 +0300

    Update TRI.h

--- Shapes/TRI.h
+++ Shapes/TRI.h
@@ -9,9 +9,11 @@ private:
 	Point Corner2;
 	Point Corner3;
 public:
-	TRI(Point, Point, Point, GfxInfo shapeGfxInfo );
-	virtual ~TRI();
-	virtual void Draw(GUI* pUI) const;
+TRI(Point, Point, Point, GfxInfo shapeGfxInfo );
+virtual ~TRI();
+virtual void Draw(GUI* pUI) const;
+virtual bool isPointInside(int x, int y) const override;
+virtual void setSelected(bool selected) override;
 };
 
 
