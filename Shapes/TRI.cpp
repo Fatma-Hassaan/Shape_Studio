048ad8900acd1b43e2d9d92cafb3dbafdd21482a
fatma hassaan <138074800+Fatma-Hassaan@users.noreply.github.com>
2024-07-31 15:32:23 +0300

    Update TRI.cpp

--- Shapes/TRI.cpp
+++ Shapes/TRI.cpp
@@ -14,3 +14,16 @@ void TRI::Draw(GUI* pUI) const
 {
 	pUI->DrawTRI(Corner1, Corner2,Corner3, ShpGfxInfo);
 }
+bool TRI::isPointInside(int x, int y) const {
+    return (x >= Corner1.x && x <= Corner2.x && y >= Corner1.y && y <= Corner2.y);
+}
+
+void TRI::setSelected(bool selected) {
+    ShpGfxInfo.isSelected = selected;
+    if (selected) {
+        ShpGfxInfo.DrawClr = SELECTED_BORDER_COLOR; // Change border color to indicate selection
+    }
+    else {
+        ShpGfxInfo.DrawClr = ORIGINAL_BORDER_COLOR;  // Revert to original border color
+    }
+}
