6f8123292bdbe702aba6c12d65a10f5053a263f2
fatma hassaan <138074800+Fatma-Hassaan@users.noreply.github.com>
2024-07-31 15:24:09 +0300

    Update LINE.cpp

--- Shapes/LINE.cpp
+++ Shapes/LINE.cpp
@@ -13,3 +13,16 @@ void LINE::Draw(GUI* pUI) const
 {	
 	pUI->DrawLINE(Corner1, Corner2, ShpGfxInfo);
 }
+bool Line::isPointInside(int x, int y) const {
+    return (x >= Corner1.x && x <= Corner2.x && y >= Corner1.y && y <= Corner2.y);
+}
+
+void Line::setSelected(bool selected) {
+    ShpGfxInfo.isSelected = selected;
+    if (selected) {
+        ShpGfxInfo.DrawClr = SELECTED_BORDER_COLOR; // Change border color to indicate selection
+    }
+    else {
+        ShpGfxInfo.DrawClr = ORIGINAL_BORDER_COLOR;  // Revert to original border color
+    }
+}
