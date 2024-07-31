f7ed54d0e231bc342cc42382d45ba5f5b11f6601
fatma hassaan <138074800+Fatma-Hassaan@users.noreply.github.com>
2024-07-31 15:26:38 +0300

    Update Rect.cpp

--- Shapes/Rect.cpp
+++ Shapes/Rect.cpp
@@ -14,3 +14,16 @@ void Rect::Draw(GUI* pUI) const
 	//Call Output::DrawRect to draw a rectangle on the screen	
 	pUI->DrawRect(Corner1, Corner2, ShpGfxInfo);
 }
+bool Rect::isPointInside(int x, int y) const {
+    return (x >= Corner1.x && x <= Corner2.x && y >= Corner1.y && y <= Corner2.y);
+}
+
+void Rect::setSelected(bool selected) {
+    ShpGfxInfo.isSelected = selected;
+    if (selected) {
+        ShpGfxInfo.DrawClr = SELECTED_BORDER_COLOR; // Change border color to indicate selection
+    }
+    else {
+        ShpGfxInfo.DrawClr = ORIGINAL_BORDER_COLOR;  // Revert to original border color
+    }
+}
