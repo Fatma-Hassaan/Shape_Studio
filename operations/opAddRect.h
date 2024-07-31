8ee4b1bf703a9694690c636b1dfc2fcd3c0f8eed
fatma hassaan <138074800+Fatma-Hassaan@users.noreply.github.com>
2024-07-31 14:23:35 +0300

    Update opAddRect.h

--- operations/opAddRect.h
+++ operations/opAddRect.h
@@ -1,16 +1,1 @@
-#pragma once
-
-#include "operation.h"
-
-//Add Rectangle operation class
-class opAddRect: public operation
-{
-public:
-	opAddRect(controller *pCont);
-	virtual ~opAddRect();
-	
-	//Add rectangle to the controller
-	virtual void Execute() ;
-	
-};
 
