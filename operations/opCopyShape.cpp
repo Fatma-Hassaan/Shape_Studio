c528fb8e2557dd1747d4113b72dd4dc3011332f9
fatma hassaan <138074800+Fatma-Hassaan@users.noreply.github.com>
2024-08-08 09:44:03 +0300

    Update opCopyShape.cpp

--- operations/opCopyShape.cpp
+++ operations/opCopyShape.cpp
@@ -1,6 +1,7 @@
 #include "opCopyShape.h"
 #include "..\controller.h"
-
+#include"GUI/GUI.h"
+#include"Shapes/Shape.h"
 opCopyShape::opCopyShape(controller* pCont) : operation(pCont) {}
 
 void opCopyShape::Execute()
@@ -8,18 +9,15 @@ void opCopyShape::Execute()
     GUI* pUI = pControl->GetUI();
     Graph* pGraph = pControl->getGraph();
     
-    int x, y;
-    pUI->PrintMessage("Select a shape to copy");
-    pUI->GetPointClicked(x, y);
-    
-    shape* pShp = pGraph->Getshape(x, y);
-    if (pShp != nullptr)
-    {
-        pGraph->SetClipboard(pShp->Clone());
-        pUI->PrintMessage("Shape copied");
-    }
-    else
-    {
-        pUI->PrintMessage("No shape selected");
-    }
+   	shape* cop = pGraph->GetSelected();
+	if (cop) {
+		pGraph->setCopied(cop);
+		pUI->PrintMessage("shape is Copied");
+		
+	}
+	else {
+		pUI->PrintMessage("Please Select A shape First");
+	}
+
+}
 }
